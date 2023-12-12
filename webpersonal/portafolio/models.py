53fb1070334fb631be72d7a1662ba0f3b757aced
unknown <axelcondori78@gmail.com>
2023-12-12 02:49:06 -0400

    Base de datos

--- webpersonal/portafolio/models.py
+++ webpersonal/portafolio/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class project(models.Model):
-    title = models.CharField()
+class Project(models.Model):
+    """Represents a project in the portfolio."""
+    title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='projects/')
     created = models.DateTimeField(auto_now_add=True)
-    update = models.DateTimeField(auto_now_add=True)
-    
+    updated = models.DateTimeField(auto_now=True)
