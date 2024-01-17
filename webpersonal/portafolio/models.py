6aa7dd5abae7594c3e03a0f5a632daeeb249715b
unknown <axelcondori78@gmail.com>
2024-01-17 17:26:52 -0400

    Primer commit

--- webpersonal/portafolio/models.py
+++ webpersonal/portafolio/models.py
@@ -1,10 +1,21 @@
+"""Este módulo define los modelos de la aplicación portafolio."""
+
 from django.db import models
 
-# Create your models here.
 class Project(models.Model):
-    """Represents a project in the portfolio."""
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField(upload_to='projects/')
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    """Representa un proyecto dentro del portafolio."""
+    title = models.CharField(max_length=200, verbose_name="Título")
+    description = models.TextField(verbose_name="Descripción")
+    image = models.ImageField(verbose_name="Imagen")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
+
+    class Meta:
+        """Información de metadatos para el modelo Project."""
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = ["-created"]
+
+    def __str__(self):
+        """Devuelve la representación en cadena del modelo."""
+        return str(self.title)
